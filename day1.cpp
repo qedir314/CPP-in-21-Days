0f213d62125d8c297d423bde3fa46ffb320d310d
qedir314 <qedirrehmetov252@yahoo.com>
2024-09-01 22:36:52 +0400

    first commit

---
+++ day1.cpp
@@ -0,0 +1,15 @@
+//
+// Created by USER on 9/1/2024.
+//
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello World \n";
+
+    int x = 5;
+    int y = 7;
+    std::cout << std::endl;
+    std::cout << x + y << " " << x * y;
+    return 0;
+}
